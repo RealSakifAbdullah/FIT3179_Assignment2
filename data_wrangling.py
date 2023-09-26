bc4c7b6afb49df5e7871e7d1e208522c11db799c
MangoSyrup <mangowlia@gmail.com>
2023-09-27 00:11:34 +1000

    commit

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
 # Replace 'your_file.csv' with the path to your CSV file
-df = pd.read_csv('your_file.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/RealSakifAbdullah/FIT3179_Assignment2/main/data/missing_migrants.csv')
 
-region_sum = df.groupby('Region of Incidents')['Column_to_sum'].sum()
+region_counts = df['Region of Incident'].value_counts()
 
-print(region_sum)
+# Create a DataFrame from the Series
+df_counts = region_counts.reset_index()
+df_counts.columns = ['Region of Incident', 'Count']
+
+# Specify the file path where you want to save the CSV file
+csv_file_path = 'region_counts.csv'
+
+# Save the DataFrame to a CSV file
+df_counts.to_csv(csv_file_path, index=False)
+
+# Now you have a CSV file named 'region_counts.csv' with the counts
