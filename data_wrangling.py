da32e55ae42981309e26ee589b03a8cdeac2ea07
MangoSyrup <mangowlia@gmail.com>
2023-09-29 02:13:10 +1000

    updates

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,33 +1,46 @@
 import pandas as pd
 
 # # Load your existing CSV file into a DataFrame
-df = pd.read_csv('https://raw.githubusercontent.com/RealSakifAbdullah/FIT3179_Assignment2/main/data/missing_migrants.csv')
+df = pd.read_csv('data/missing_migrants.csv')
+print(df.head())
 
-# Define a function to map region to continent
-def map_region_to_continent(region):
-    if 'Asia' in region:
-        return 'Asia'
-    
-    elif region == 'Europe':
-        return 'Europe'
-    
-    elif region == 'South America':
-        return 'South America'
+value_counts = df['RegionWB of Incident'].value_counts().reset_index()
 
-    elif region in ['North America','Caribbean','Central America']:
-        return 'North America'
-    
-    elif 'Africa' in region:
-        return 'Africa'
-    
-    elif region == 'Mediterranean':
-        return 'Seven seas (open ocean)'
-    
-    else:
-        return None  # Handle other cases if needed
+# Rename the columns in the new DataFrame
+value_counts.columns = ['RegionWB of Incident', 'Count']
 
-# Apply the mapping function to create the new column
-df['Continent of Incident'] = df['Region of Incident'].apply(map_region_to_continent)
+# Display the new DataFrame
+print(value_counts)
 
-# Save the modified DataFrame to a new CSV file
-df.to_csv('test.csv', index=False)
+# # Define a function to map region to continent
+# def map_region_to_continent(region):
+#     if region == 'Southern Asia' :
+#         return 'South Asia'
+
+#     elif region == 'Europe':
+#         return 'Europe & Central Asia'
+    
+#     elif region == 'South America':
+#         return 'South America'
+    
+#     elif region in ['Caribbean','Central America','South America']:
+#         return 'Latin America & Caribbean'
+
+#     elif region in ['North America']:
+#         return 'North America'
+    
+#     elif region in ['Eastern Africa','Mediterranean','Western Africa']:
+#         return 'Sub-Saharan Africa'
+
+
+#     elif region in ['Northern Africa','Western Asia','Middle Africa','Southern Africa']:
+#         return "Middle East & North Africa"
+    
+#     else:
+#         return None  # Handle other cases if needed
+
+# # Apply the mapping function to create the new column
+# df['RegionWB of Incident'] = df['Region of Incident'].apply(map_region_to_continent)
+
+# # Save the modified DataFrame to a new CSV file
+value_counts.to_csv('regionwb_counts.csv', index=False)
